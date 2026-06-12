c160d721c0a656cc228dc5e472691a496acc2eff
Fitrah-Andhika-Ramadhan <fitrahramdhan31@gmail.com>
2026-06-12 19:10:22 +0700

    fix: nextauth secret and trustHost for Vercel deployment

--- lib/auth.ts
+++ lib/auth.ts
@@ -92,5 +92,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       return session
     },
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET,
+  trustHost: true,
 })
